c007b2185d5fc671912bf22f4cb61068676287ef
Diego Isquierdo <diego.isquierdo@hotmail.com>
2020-06-01 22:25:35 -0300

    Add recursos de paginacao

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport //auxilia no suporte do spring ao pegar n URL os dados de paginação 'Pageable'
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,11 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -37,13 +43,27 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
+	//utlizado classe Page ao inves do 'List' para facilitar a informação das páginas..
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) { 
+					//Pageable > pega da URL os parâmetros de paginacao (em Inglês 'page''size''sort','asc/desc')
+					//localhost:8080/topicos?page=0&size=4&sort=id,asc (asc||desc)
+			//organizando por mais de uma organização
+			//localhost:8080/topicos?page=0&size=4&sort=id,asc&sort=dataCriacao,desc
+			//@PageableDefault(sort = "id", direction = Direction.DESC) > define a paginação padrão
+			//se vier parametros de ordenação, os parâmetros eh q definem..
+		
+		
+		
+		//criando recurso de paginção	//ordenção ASCendente/DESCendente | String de ordenacao
+		//Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
+		
 		if (nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos);
 		} else {
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 			return TopicoDto.converter(topicos);
 		}
 	}
